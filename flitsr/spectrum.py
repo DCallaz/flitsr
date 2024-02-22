bec7c73f25f17b84d419ce79f508b3e5e8f32cb7
Dylan Callaghan <21831599@sun.ac.za>
2024-02-22 12:41:44 +0200

    Began refactoring spectrum table.
    [Details]
    As a part of the flitsr refactoring, the table used to hold the spectrum
    for the program needs to be refactored to be an object. This will help
    with the program understanding and further usage. The object that will
    be used for the spectrum is created in this commit, but still needs work
    before it can be used.
    
    [File's changed]
    
    	new file:   spectrum.py

---
+++ flitsr/spectrum.py
@@ -0,0 +1,22 @@
+class Spectrum():
+    """
+    An implementation for a program spectrum, represented in tabular form.
+    """
+    class Test():
+        """
+        A test object holds all of the spectral information pertaining to a
+        particular test.
+        """
+
+        def __init__(self, name, outcome):
+            self.name = name
+            self.outcome = outcome
+            self.spectra = dict()
+
+    def __init__(self):
+        # TODO
+        pass
+
+    def __getitem__(self, i):
+        # TODO
+        pass
