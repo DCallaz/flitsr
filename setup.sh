3a33b91a8fe2a5cd125d49d6ee67e2fcd81a4b5d
Dylan Callaghan <21831599@sun.ac.za>
2024-03-18 10:57:10 +0200

    Fixed up setup.sh script.
    [Details]
    Added checks for python at the beginning of the setup script, as well as
    installation of numpy and conditional bashrc editing.
    
    [File's changed]
    	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "export FLITSR_HOME=\"$SCRIPT_DIR\"" >> ~/.bashrc
-echo "export PATH=\"\$FLITSR_HOME/bin:\$PATH\"" >> ~/.bashrc
+# check if python is installed
+python --version &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "ERROR: python not installed. Please install python and rerun this script."
+fi
+# add the script path to the bashrc
+if [ "$(grep "FLITSR_HOME" ~/.bashrc)" == "" ]; then
+  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  echo "export FLITSR_HOME=\"$SCRIPT_DIR\"" >> ~/.bashrc
+  echo "export PATH=\"\$FLITSR_HOME/bin:\$PATH\"" >> ~/.bashrc
+fi
+# install numpy
+pip install numpy
