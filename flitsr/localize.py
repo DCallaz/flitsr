fac1cfc7bfc594b945166976ac0893ba971d58fd
Dylan Callaghan <21831599@sun.ac.za>
2024-03-19 09:41:01 +0200

    Adapted the scoring to use spectrum

--- flitsr/localize.py
+++ flitsr/localize.py
@@ -1,69 +1,43 @@
 from suspicious import Suspicious
+from spectrum import Spectrum
+from typing import List, Tuple
 import random
-
-def get_counts(table):
-    """
-    Returns an array of the counts for each location in the following format:
-        Location 1     ...
-    [[ep, ef, np, nf], ...]
-    """
-    counts = [[0 for i in range(4)] for j in range(len(table[0])-2)]
-    for i in range(0, len(table)):
-        if (table[i][0] == False):
-            continue
-        for j in range(2, len(table[i])):
-            if (table[i][j]):#location was executed
-                if (table[i][1]):#Test case passed
-                    counts[j-2][0] += 1#ep++
-                else:#Test case failed
-                    counts[j-2][1] += 1#ef++
-            else:#location not executed
-                if (table[i][1]):#Test case passed
-                    counts[j-2][2] += 1#np++
-                else:#Test case failed
-                    counts[j-2][3] += 1#nf++
-    return counts
-
-def apply_formula(counts, formula):
-    scores = []
-    for i in range(0, counts["locs"]):
-        sus = Suspicious(counts["f"][i], counts["tf"], counts["p"][i], counts["tp"])
-        scores.append(sus.execute(formula))
-    return scores
-
-def get_exec(counts):
-    execs = []
-    for i in range(counts["locs"]):
-       execs.append(counts["p"][i]+counts["f"][i])
-    return execs
 
 orig = None
 
-def sort(zipped, order, tiebrk):
-    if (tiebrk == 1):#Sorted by execution counts
-        sort = sorted(zipped, key=lambda x: x[2], reverse=order)
+
+def apply_formula(spec: Spectrum, formula: str):
+    scores: List[Tuple[Spectrum.Element, float, float]] = []
+    for elem in spec.elements:
+        sus = Suspicious(spec.f[elem], spec.tf, spec.p[elem], spec.tp)
+        exect = spec.p[elem]+spec.f[elem]
+        scores.append((sus.execute(formula), elem, exect))
+    return scores
+
+
+def sort(scores, order, tiebrk):
+    if (tiebrk == 1):  # Sorted by execution counts
+        sort = sorted(scores, key=lambda x: x[2], reverse=order)
         sort = sorted(sort, key=lambda x: x[0], reverse=order)
-    elif (tiebrk == 2):#random ordering
-        random.shuffle(zipped)
-        sort = sorted(zipped, key=lambda x: x[0], reverse=order)
-    elif (tiebrk == 3):#original ranking tie break
-        if (orig != None):#sort by original ranking, then execution count
-            sort = sorted(zipped, key=lambda x: orig[x[1]][2], reverse=order)
-            #print("sort1:",sort)
+    elif (tiebrk == 2):  # random ordering
+        random.shuffle(scores)
+        sort = sorted(scores, key=lambda x: x[0], reverse=order)
+    elif (tiebrk == 3):  # original ranking tie break
+        if (orig is not None):  # sort by original ranking then execution count
+            sort = sorted(scores, key=lambda x: orig[x[1]][2], reverse=order)
             sort = sorted(sort, key=lambda x: orig[x[1]][0], reverse=order)
-            #print("sort2:",sort)
-        else:#if no orig, still sort by current execution count
-            sort = sorted(zipped, key=lambda x: x[2], reverse=order)
+        else:  # if no orig, still sort by current execution count
+            sort = sorted(scores, key=lambda x: x[2], reverse=order)
         sort = sorted(sort, key=lambda x: x[0], reverse=order)
-        #print("sort3:",sort)
     else:
-        sort = sorted(zipped, key=lambda x: x[0], reverse=order)
+        sort = sorted(scores, key=lambda x: x[0], reverse=order)
     return sort
 
-#Assumes the table is not empty
-def localize(counts, formula, tiebrk, order=True):
-    #counts = get_counts(table)
-    scores = apply_formula(counts, formula)
-    execs = get_exec(counts)
-    zipped = list(map(list, zip(scores, range(counts["locs"]), execs)))
-    return sort(zipped, order, tiebrk)
+
+def localize(spectrum, formula, tiebrk, order=True):
+    """
+    Calculate the scores for each of the elements using the given formula.
+    Assumes a non-empty spectrum.
+    """
+    scores = apply_formula(spectrum, formula)
+    return sort(scores, order, tiebrk)
