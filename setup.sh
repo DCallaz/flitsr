6129927b37d8762d3668660b7eabbebfad235474
Dylan Callaghan <21831599@sun.ac.za>
2024-03-19 11:21:45 +0200

    Made the setup script shell un-specific.
    [Details]
    Added checks to the setup script to ensure that the script will work on
    the current (else the default) shell. This should allow the setup script
    to be run on any shell type.
    
    [File's changed]
    	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,25 @@ python --version &> /dev/null
 if [ $? -ne 0 ]; then
   echo "ERROR: python not installed. Please install python and rerun this script."
 fi
-# add the script path to the bashrc
-if [ "$(grep "FLITSR_HOME" ~/.bashrc)" == "" ]; then
+# add the script path to the bashrc/zshrc
+shell="$(ps -p$PPID | grep -v "PID" | awk '{print $4}')"
+if [[ ! "$shell" =~ ^sh|csh|tcsh|ksh|dash|bash|zsh|fish$ ]]; then
+  shell="$(basename $SHELL)"
+fi
+echo "$shell"
+exit 0
+if [ "$shell" == "bash" ]; then
+  rcfile=~/.bashrc
+elif [ "$shell" == "zsh" ]; then
+  rcfile=~/.zshrc
+else
+  rcfile=~/.profile
+fi
+if [ "$(grep "FLITSR_HOME" "$rcfile")" == "" ]; then
   SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-  echo "export FLITSR_HOME=\"$SCRIPT_DIR\"" >> ~/.bashrc
-  echo "export PATH=\"\$FLITSR_HOME/bin:\$PATH\"" >> ~/.bashrc
-  echo "~/.bashrc has been updated. Run 'source ~/.bashrc' to update your session."
+  echo "export FLITSR_HOME=\"$SCRIPT_DIR\"" >> "$rcfile"
+  echo "export PATH=\"\$FLITSR_HOME/bin:\$PATH\"" >> "$rcfile"
+  echo "$rcfile has been updated. Run 'source $rcfile' to update your session."
 fi
 # install numpy
 pip install numpy
