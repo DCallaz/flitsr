9a6d91e1532ee3bb13fd764c621cbf6d521361d3
Dylan Callaghan <21831599@sun.ac.za>
2024-03-25 14:54:21 +0200

    Updated RC configuration for setup script.
    [Details]
    Changed the setup script to always update the FLITSR setup in the RC
    file, but to preserve the RC file structure when doing so. These
    changes will check for FLITSR_HOME in the RC file, and if found, will
    replace these lines with the flitsr setup lines. If not found, the
    lines will be placed at the end of the file.
    
    [File's changed]
    	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -16,14 +16,20 @@ elif [ "$shell" == "zsh" ]; then
 else
   rcfile=~/.profile
 fi
-if [ "$(grep "FLITSR_HOME" "$rcfile")" == "" ]; then
-  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-  export FLITSR_HOME="$SCRIPT_DIR"
-  echo "export FLITSR_HOME=\"$SCRIPT_DIR\"" >> "$rcfile"
-  echo "export PYTHONPATH=\"\$FLITSR_HOME:\$PYTHONPATH\"" >> "$rcfile"
-  echo "export PATH=\"\$FLITSR_HOME/bin:\$PATH\"" >> "$rcfile"
-  echo "$rcfile has been updated. Run 'source $rcfile' to update your session."
+if [ ! -f "$rcfile" ]; then
+  echo "# FLITSR setup" > "$rcfile"
 fi
+line_num="$(wc -l "$rcfile" | awk '{print $1}')"
+if [ "$(grep "FLITSR_HOME" "$rcfile")" != "" ]; then
+  line_num="$(grep -n "FLITSR_HOME" "$rcfile" | head -n1 | awk -F':' '{print $1-1}')"
+  sed -i '/FLITSR_HOME/d' "$rcfile"
+fi
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export FLITSR_HOME="$SCRIPT_DIR"
+sed -i "${line_num}a export PATH=\"\$FLITSR_HOME/bin:\$PATH\"" "$rcfile"
+sed -i "${line_num}a export PYTHONPATH=\"\$FLITSR_HOME:\$PYTHONPATH\"" "$rcfile"
+sed -i "${line_num}a export FLITSR_HOME=\"$SCRIPT_DIR\"" "$rcfile"
+echo "$rcfile has been updated. Run 'source $rcfile' to update your session."
 # install numpy
 python -m venv "$FLITSR_HOME/.venv"
 source "$FLITSR_HOME/.venv/bin/activate"
