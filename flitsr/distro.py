03ff6022b103eaea0a1d23d58f5d7c396b4cc895
Dylan Callaghan <21831599@sun.ac.za>
2024-06-04 12:39:53 +0200

    Fixed up the distro class

--- flitsr/distro.py
+++ flitsr/distro.py
@@ -1,13 +1,6 @@
 import sys
 from matplotlib import pyplot as plt
 
-if __name__ == "__main__":
-    x,y = distro(sys.argv[1])
-    plt.bar(x, y)
-    plt.title("Fault distribution for project "+str(sys.argv[2]))
-    plt.xlim([0, 32])
-    plt.ylim([0, 1200])
-    plt.show()
 
 def distro(file):
     lines = open(file).readlines()
@@ -19,4 +12,13 @@ def distro(file):
             vals = line.split()
             x.append(int(vals[0]))
             y.append(int(vals[1]))
-    return x,y
+    return x, y
+
+
+if __name__ == "__main__":
+    x, y = distro(sys.argv[1])
+    plt.bar(x, y)
+    plt.title("Fault distribution for project "+str(sys.argv[2]))
+    plt.xlim([0, 32])
+    plt.ylim([0, 1200])
+    plt.show()
