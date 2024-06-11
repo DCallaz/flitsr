39690a78a263164cb8f74505b0a755939d03df51
Dylan Callaghan <21831599@sun.ac.za>
2024-06-11 16:45:43 +0200

    Changed argument handling to argparse.
    [Details]
    Refactored the argument handling to be managed by argparse instead of
    manually. This makes a much better and well formatted help message, as
    well as making the argument handling easier to handle. The handling has
    been moved to a separate file (the args file), which allows separation
    of concerns.
    
    [File's changed]
    	new file:   args.py
    	modified:   flitsr.py

---
+++ flitsr/args.py
@@ -0,0 +1,188 @@
+import argparse
+import sys
+from flitsr.suspicious import Suspicious
+from flitsr.cutoff_points import cutoff_points
+
+
+def parse_args(args):
+    # General options
+    parser = argparse.ArgumentParser(prog='flitsr', description='An automatic '
+            'fault finding/localization tool for multiple faults.')
+    parser.add_argument('input', help='The coverage file (TCM) or '
+                        'directory (GZoltar) containing the coverage collected'
+                        ' for the system over the test suite')
+    parser.add_argument('-m', '--metric', action='store',
+            choices=Suspicious.getNames(), default='ochiai', metavar='METRIC',
+            help='The underlying (SBFL) metric to use when either ranking '
+            '(if the sbfl option is given), or running the FLITSR algorithm.'
+            ' Allowed values are: ['+', '.join(Suspicious.getNames())+']'
+            ' (default: %(default)s)')
+    parser.add_argument('-s', '--sbfl', action='store_true',
+            help='Disables the FLITSR algorithm so that only the base metric '
+            'is used to produce the ranking. This is equivalent to using the '
+            'base metric as-is, but allows the user to run these metrics '
+            'within the FLITSR framework')
+    parser.add_argument('-*', '--multi', action='store_true',
+            help='Runs the FLITSR* (i.e. multi-round) algorithm')
+    parser.add_argument('-r', '--ranking', action='store_true',
+            help='Changes flitsr\'s expected input to be an SBFL ranking in '
+            'Gzoltar or FLITSR format (determined automatically), instead of '
+            'the usual coverage, and produces the specified calculations '
+            '(or just the ranking if no calculations are given). NOTE: any '
+            'non-output based options will be ignored with this option')
+    parser.add_argument('-c', '--method', action='store_true',
+            help='The default for FLITSR is to use the collected coverage '
+            'as-is and merely produce the ranking in terms of the names/labels'
+            ' given to the elements. Alternatively, using this option, FLITSR '
+            'can assume the coverage given is a statement level coverage, and '
+            'will attempt to collapse this coverage to produce a method level '
+            'coverage result. This collapse is done by constructing a '
+            'coverage matrix with only the method names, where the execution '
+            'of a method is determined by the union of the executions of its '
+            'statements. Bugs added to the coverage are handled in a similar '
+            'fashion.')
+    parser.add_argument('--split', help='When given, this option causes faults'
+            ' that are a combination of two or more sub-faults in mutually'
+            ' exclusive parts of the system to be split into separate'
+            ' identified faults. As a by-product this also drops faults that'
+            ' are not exposed by failing tests', action='store_true')
+    parser.add_argument('--collapse', action='store_true',
+            help='Collapse dynamic basic-block groups into singular elements '
+            'for the ranking an calculations')
+    parser.add_argument('-a', '--all', action='store_true',
+            help='Used in the evaluation of FLITSR against other techniques. '
+            'Runs all metrics given in suspicious.py and both FLITSR and '
+            'FLITSR* extensions over each metric. Also enables all of the '
+            'above evaluation calculations. Prints the results out to files '
+            'named [<flitsr method>_]<metric>.results for each FLITSR method '
+            'and metric')
+    parser.add_argument('-d', '--decimals', action='store', type=int,
+            default=2, help='Sets the precision (number of decimal points) for '
+            'the output of all of the calculations (default: %(default)s)')
+
+    # Tie breaking options
+    tie_grp = parser.add_argument_group('Tie breaking strategy',
+            'Specifies the tie breaking strategy to use for FLITSR and the '
+            'localization')
+    tie_grp.add_argument('--tiebrk', action='store_const', const=1,
+            default=3, help='Breaks ties using only execution counts')
+    tie_grp.add_argument('--rndm', action='store_const', const=2, dest='tiebrk',
+            help='Breaks ties by a randomly ordering')
+    tie_grp.add_argument('--otie', action='store_const', const=3, dest='tiebrk',
+            help='Breaks ties by using the original base metric ranking (in '
+            'the case of FLITSR) and by execution counts otherwise')
+
+    # Calculation options
+    calc_grp = parser.add_argument_group('Calculations',
+            'The following arguments replace the default ranking output of '
+            'FLITSR with evaluation calculations. Multiple of the following '
+            'arguments can be given in the same call')
+
+    # Wasted effort calcuation options
+    calc_grp.add_argument('--first', dest='weff', action='append_const',
+            default=[], const='first',
+            help='Display the wasted effort to the first fault')
+    calc_grp.add_argument('--avg', '--average', dest='weff',
+            action='append_const', const='avg',
+            help='Display the wasted effort to the average fault')
+    calc_grp.add_argument('--med', '--median', dest='weff',
+            action='append_const', const='med',
+            help='Display the wasted effort to the median fault')
+    calc_grp.add_argument('--last', dest='weff', action='append_const',
+            const='last', help='Display the wasted effort to the last fault')
+
+    # TOP1 calculation options
+    calc_grp.add_argument('--one-top1', dest='top1',
+            action='append_const', const='one', default=[],
+            help='Display a boolean value indicating whether at least one '
+            'fault was found in the TOP1 group (elements with the highest '
+            'suspiciousness')
+    calc_grp.add_argument('--all-top1', dest='top1',
+            action='append_const', const='all',
+            help='Display the number of faults found in the top1 group')
+    calc_grp.add_argument('--perc-top1', dest='top1',
+            action='append_const', const='perc',
+            help='Display the percentage of faults found in the top1 group')
+
+    # Percent at n calculation options
+    calc_grp.add_argument('--perc@n', '--percent-at-n', dest='perc_at_n',
+            action='append_const', const='perc', default=[],
+            help='Produces the percentage-at-N values (i.e. the percentage of '
+            'faults found at N%% of code inspected). The output of the perc@n '
+            'calculation is a list of ranks of all found faults, preceded by '
+            'the number of elements in the system, which can be used to '
+            'generate percentage-at-N/recall graphs')
+    calc_grp.add_argument('--auc', '--area-under-curve', dest='perc_at_n',
+            action='append_const', const='auc',
+            help='Dislpays the area under the curve produced by the '
+            'percentage-at-N calculation')
+    calc_grp.add_argument('--pauc', '--percent-area-under-curve',
+            dest='perc_at_n', action='append_const', const='pauc',
+            help='Dislpays the area under the curve '
+            'produced by the percentage-at-N calculation as a percentage of '
+            'the maximum possible value (i.e. closest to perfect recall)')
+    calc_grp.add_argument('--lauc', '--log-area-under-curve',
+            dest='perc_at_n', action='append_const', const='lauc',
+            help='Dislpays the area under the curve '
+            'produced by the percentage-at-N calculation as a logarithmic '
+            'percentage of the maximum possible value (i.e. closest to perfect '
+            'recall). The logarithmic effect causes lower ranks to have a '
+            'greater effect on the value, which corresponds to the lower ranks '
+            'being more useful to the developer')
+
+    # Precision recall type functions
+    def pr(value):
+        if (value == "b" or value == "f" or value.isdigit()):
+            return value
+        else:
+            raise ValueError(str(value)+" is not a valid precision/recall "
+                    "string value")
+    def precision(value):
+        return ('p', pr(value))
+    def recall(value):
+        return ('r', pr(value))
+
+    # Precision recall calculation options
+    calc_grp.add_argument('--precision-at', dest='prec_rec', metavar='x',
+            action='append', default=[], type=precision,
+            help='Displays precision values at a given rank `x`. Precision '
+            'is the amount of faults f found within the cut-off point `x`, '
+            'out of the number of elements seen (i.e. f/x). Can be specified '
+            'multiple times')
+    calc_grp.add_argument('--recall-at', dest='prec_rec', metavar='x',
+            action='append', default=[], type=recall,
+            help='Displays recall values at a given rank `x`. Recall '
+            'is the amount of faults f found within the cut-off point `x`, '
+            'out of the total number of faults n (i.e. f/n). Can be specified '
+            'multiple times')
+
+    # TODO: Add parallel args when implemented
+    # parallel_opts = ['bdm', 'msp', 'hwk', 'vwk']
+    # parser.add_argument('-p', '--parallel', action='store',
+    #         choices=parallel_opts, metavar='ALGORITHM',
+    #         help='Run one of the parallel debugging algorithms on the spectrum '
+    #         'to produce multiple spectrums, and process all other options on '
+    #         'each spectrum. Allowed values are: ['+', '.join(parallel_opts)+']')
+
+    # TODO: Add OBA and worst args if necessary
+    cut_eval_opts=['worst', 'best', 'resolve']
+    parser.add_argument('--cutoff-eval', action='store', metavar='MODE',
+            choices=cut_eval_opts, help='Specifies the performance mode to use '
+            'when using a multi-fault fixing cut-off strategy to produce '
+            'rankings. Allowed values are: ['+', '.join(cut_eval_opts)+']'+
+            ' (default %(default)s)', default='best')
+    cutoff_opts = cutoff_points.getNames()
+    parser.add_argument('--cutoff-strategy', action='store', metavar='STRATEGY',
+            choices=cutoff_opts, help='Cuts off the ranking using the given '
+            'strategy\'s cut-off point. This affects both the rank output '
+            'method and any calculations. Allowed values are: ['+
+            ', '.join(cutoff_opts)+'] (default %(default)s). '
+            'For basis, an optional value n may be given (e.g. basis=n) '
+            'that determines the number of bases included before the cutoff')
+
+    args = parser.parse_args(args)
+    return args
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    print(args)
--- flitsr/flitsr.py
+++ flitsr/flitsr.py
@@ -1,9 +1,10 @@
 import sys
 import re
 import copy
+from argparse import Namespace
 from os import path as osp
 from math import log
-from typing import List, Set
+from typing import List, Set, Optional
 from flitsr import weffort
 from flitsr import top
 from flitsr import percent_at_n
@@ -14,6 +15,7 @@ from flitsr.suspicious import Suspicious
 from flitsr.cutoff_points import cutoff_points
 from flitsr.spectrum import Spectrum
 from flitsr import score
+from flitsr.args import parse_args
 
 
 def remove_from_tests(element: Spectrum.Element, spectrum: Spectrum):
@@ -123,8 +125,9 @@ def compute_cutoff(cutoff, sort, spectrum, mode, effort=2):
     return sort
 
 
-def output(scores, spectrum, weff=None, top1=None, perc_at_n=False,
-           prec_rec=None, collapse=False, file=sys.stdout, decimals=2):
+def output(scores: score.Scores, spectrum: Spectrum, weff=[], top1=[],
+           perc_at_n=[], prec_rec=[], collapse=False, file=sys.stdout,
+           decimals=2):
     if (weff or top1 or perc_at_n or prec_rec):
         faults = find_faults(spectrum)
         if (weff):
@@ -164,21 +167,20 @@ def output(scores, spectrum, weff=None, top1=None, perc_at_n=False,
         if (perc_at_n):
             bumps = percent_at_n.getBumps(faults, scores, spectrum,
                                           collapse=collapse)
-            if (perc_at_n == 1):
+            auc = percent_at_n.auc_calc(percent_at_n.combine([(bumps[0],bumps[1:])]))
+            if ('perc' in perc_at_n):
                 form = ','.join(['{{:.{}f}}'.format(decimals)]*len(bumps))
                 print("percentage at n:", form.format(*bumps), file=file)
-            else:
-                auc = percent_at_n.auc_calc(percent_at_n.combine([(bumps[0],bumps[1:])]))
-                if (perc_at_n == 2):
-                    print("auc:", auc, file=file)
-                elif (perc_at_n == 3):
-                    optimal = percent_at_n.auc_calc([(0.0, 100.0)])
-                    print("pauc:", "{:.{}f}".format(auc/optimal, decimals),
-                          file=file)
-                elif (perc_at_n == 4):
-                    optimal = percent_at_n.auc_calc([(0.0, 100.0)])+1
-                    lauc = abs(1-log(optimal-auc, optimal))
-                    print("lauc:", "{:.{}f}".format(lauc, decimals), file=file)
+            if ('auc' in perc_at_n):
+                print("auc:", auc, file=file)
+            if ('pauc' in perc_at_n):
+                optimal = percent_at_n.auc_calc([(0.0, 100.0)])
+                print("pauc:", "{:.{}f}".format(auc/optimal, decimals),
+                      file=file)
+            if ('lauc' in perc_at_n):
+                optimal = percent_at_n.auc_calc([(0.0, 100.0)])+1
+                lauc = abs(1-log(optimal-auc, optimal))
+                print("lauc:", "{:.{}f}".format(lauc, decimals), file=file)
         if (prec_rec):
             for entry in prec_rec:
                 if (entry[0] == 'p'):
@@ -195,201 +197,85 @@ def output(scores, spectrum, weff=None, top1=None, perc_at_n=False,
         print_names(spectrum, scores, file)
 
 
-def main(argv):
-    metrics = Suspicious.getNames()
-    cutoffs = cutoff_points.getNames()
-    if (len(argv) < 2):
-        print("Usage: flitsr <input file> [<metric>] [split] [method] [worst/best/resolve]"
-                +" [sbfl] [first/avg/med/last] [one_top1/all_top1/perc_top1]"
-                +" [perc@n/auc/pauc/lauc] [precision/recall]@<x> [decimals=<x>]"
-                +" [tiebrk/rndm/otie] [multi] [parallel[=bdm/msp]] [all] [basis[=<n>]]"
-                +" "+str(cutoffs))
-        print()
-        print("Where <metric> is one of:",metrics)
-        return
-    d = argv[1]
-    metric = 'ochiai'
-    flitsr = True
-    input_m = 0
-    i = 2
-    weff = []
-    top1 = []
-    prec_rec = []
-    perc_at_n = 0
-    tiebrk = 3
-    multi = 0
-    decimals = 2
-    all = False
-    collapse = False
-    parallell = None
-    split = False
-    method = False
-    cutoff = None
-    effort = 0
-    ranking = False
-    while (True):
-        if (len(argv) > i):
-            if (argv[i] in metrics):
-                metric = argv[i]
-            elif (argv[i].startswith("basis")):
-                if ("=" in argv[i]):
-                    cutoff = argv[i]
-                else:
-                    cutoff = "basis=1"
-            elif (argv[i] in cutoffs):
-                cutoff = argv[i]
-            elif (argv[i] == "method"):
-                method = True
-            elif (argv[i] == "statement"):
-                method = False
-            elif (argv[i].startswith("parallel")):
-                if ("=" in argv[i]):
-                    parType = argv[i].split("=")[1]
-                    if (parType == "bdm" or parType == "msp"
-                        or parType == "hwk" or parType == "vwk"):
-                        parallell = parType
-                    else:
-                        print("Unknown parallel argument: ", parType)
-                else:
-                    parallell = "bdm"
-            elif (argv[i] == "split"):
-                split = True
-            elif (argv[i] == "worst"):
-                effort = 1
-            elif (argv[i] == "best"):
-                effort = 0
-            elif (argv[i] == "resolve"):
-                effort = 2
-            elif (argv[i] == "sbfl"):
-                flitsr = False
-            elif (argv[i] == "tcm"):
-                pass
-            elif (argv[i] == "first"):
-                weff.append("first")
-            elif (argv[i] == "avg"):
-                weff.append("avg")
-            elif (argv[i] == "med"):
-                weff.append("med")
-            elif (argv[i] == "last"):
-                weff.append("last")
-            elif (argv[i] == "collapse"):
-                collapse = True
-            elif (argv[i] == "one_top1"):
-                top1.append("one")
-            elif (argv[i] == "all_top1"):
-                top1.append("all")
-            elif (argv[i] == "perc_top1"):
-                top1.append("perc")
-            elif (argv[i] == "size_top1"):
-                top1.append("size")
-            elif (argv[i] == "perc@n"):
-                perc_at_n = 1
-            elif (argv[i] == "auc"):
-                perc_at_n = 2
-            elif (argv[i] == "pauc"):
-                perc_at_n = 3
-            elif (argv[i] == "lauc"):
-                perc_at_n = 4
-            elif ("precision@" in argv[i]):
-                n = argv[i].split("@")[1]
-                if (n == "b" or n == "f"):
-                    prec_rec.append(('p', n))
-                else:
-                    prec_rec.append(('p', float(n)))
-            elif ("recall@" in argv[i]):
-                n = argv[i].split("@")[1]
-                if (n == "b" or n == "f"):
-                    prec_rec.append(('r', n))
-                else:
-                    prec_rec.append(('r', float(n)))
-            elif ("decimals=" in argv[i]):
-                n = argv[i].split("=")[1]
-                decimals = int(n)
-            elif (argv[i] == "tiebrk"):
-                tiebrk = 1
-            elif (argv[i] == "rndm"):
-                tiebrk = 2
-            elif (argv[i] == "otie"):
-                tiebrk = 3
-            elif (argv[i] == "multi"):
-                multi = 1
-            elif (argv[i] == "ranking"):
-                ranking = True
-            elif (argv[i] == "all"):
-                all = True
-            else:
-                print("Unknown option:", argv[i])
-                quit()
-            i += 1
-        else:
-            break
-    if (osp.isfile(d)):
+def main(argv: List[str]):
+    args: Namespace = parse_args(argv[1:])
+    if (osp.isfile(args.input)):
         input_m = 1
-    elif (osp.isdir(d) and osp.isfile(osp.join(d, "matrix.txt"))):
+    elif (osp.isdir(args.input) and
+            osp.isfile(osp.join(args.input, "matrix.txt"))):
         input_m = 0
     else:
-        print("ERROR:", d, "does not exist")
+        print("ERROR:", args.input, "does not exist")
         quit()
-    if (cutoff and cutoff.startswith('basis')):
+    if (args.cutoff_strategy and args.cutoff_strategy.startswith('basis')):
         flitsr = True
         multi = 1
     # If only a ranking is given, print out metrics and return
-    if (ranking):
+    if (args.ranking):
         from flitsr.ranking import read_any_ranking
-        scores, spectrum = read_any_ranking(d, method_level=method)
-        output(scores, spectrum, weff, top1, perc_at_n, prec_rec,
-               collapse, decimals=decimals)
+        scores, spectrum = read_any_ranking(args.input,
+                method_level=args.method)
+        output(scores, spectrum, args.weff, args.top1, args.perc_at_n,
+                args.prec_rec, args.collapse, decimals=args.decimals)
         return
     # Else, run the full process
     if (input_m):
         from flitsr.tcm_input import read_table
-        d_p = re.sub("\\.\\w+$", ".run", d)
+        d_p = re.sub("\\.\\w+$", ".run", args.input)
     else:
         from flitsr.input import read_table
-        d_p = d.split("/")[0] + ".run"
+        d_p = args.input.split("/")[0] + ".run"
     # Read the table in and setup parallel if needed
-    spectrum = read_table(d, split, method_level=method)
+    spectrum = read_table(args.input, args.split, method_level=args.method)
     if (spectrum is None):
         print("WARNING: Incorrectly formatted input file, terminating...",
               file=sys.stderr)
         return
-    if (parallell):
-        spectrums = parallel.parallel(d, spectrum, tiebrk, metric,
-                                      parallell)
+    if (hasattr(args, 'parallel') and args.parallel):
+        # TODO: Fix parallel below
+        # spectrums = parallel.parallel(args.input, spectrum, args.tiebrk,
+        #                               args.metric, args.parallel)
+        print("Parallel not supported at the moment...")
     else:
         spectrums = [spectrum]
-    if (all):  # Run the 'all' script (do all metrics and calculations)
+    if (args.all):  # Run the 'all' script (do all metrics and calculations)
+        metrics = Suspicious.getNames()
         types = ["base_", "flitsr_", "flitsr_multi_"]
         for m in range(len(metrics)):
             for i in range(3):
                 file = open(types[i]+metrics[m]+"_"+d_p, "x")
-                if (m == 'parallel'):
-                    spectrums = parallel.parallel(d, spectrum, tiebrk, metric,
-                                                  parallell)
-                    output(sort, spectrum, weff=["first","med","last"],
-                            perc_at_n=1,prec_rec=[('p', 1), ('p', 5), ('p', 10),
-                            ('p', "f"), ('r', 1), ('r', 5), ('r', 10), ('r', "f")],
-                            collapse=collapse, file=file, decimals=decimals)
-                else:
-                    sort = run(spectrum, metrics[m], i >= 1, 3, (i == 2)*2)
-                    output(sort, spectrum, weff=["first", "avg", "med", "last"],
-                            perc_at_n=1,prec_rec=[('p', 1), ('p', 5), ('p', 10),
-                            ('p', "f"), ('r', 1), ('r', 5), ('r', 10), ('r', "f")],
-                            collapse=collapse, file=file)
+                # TODO: Decide whether to include the parallel here
+                # if (m == 'parallel'):
+                #     spectrums = parallel.parallel(args.input, spectrum,
+                #             args.tiebrk, args.metric, args.parallel)
+                #     output(sort, spectrum, weff=["first","med","last"],
+                #            perc_at_n=['perc'],prec_rec=[('p', 1), ('p', 5), ('p', 10),
+                #                                  ('p', "f"), ('r', 1), ('r', 5),
+                #                                  ('r', 10), ('r', "f")],
+                #             collapse=args.collapse, file=file, decimals=args.decimals)
+                # else:
+                sort = run(spectrum, metrics[m], i >= 1, 3, (i == 2)*2)
+                output(sort, spectrum, weff=["first", "avg", "med", "last"],
+                        perc_at_n=['perc'],prec_rec=[('p', 1), ('p', 5), ('p', 10),
+                        ('p', "f"), ('r', 1), ('r', 5), ('r', 10), ('r', "f")],
+                        collapse=args.collapse, file=file)
                 file.close()
+                # end else above
                 spectrum.reset()
     else:  # Just run the given metric and calculations
         for i, spectrum in enumerate(spectrums):
             if (i > 0):
                 print("<---------------------- Next Ranking ---------------------->")
-            sort = run(spectrum, metric, flitsr, tiebrk, multi)
+            sort = run(spectrum, args.metric, not args.sbfl, args.tiebrk,
+                       args.multi)
             # if ('map' in counts): # Map back if parallel
             #     for rank in sort:
             #         rank[1] = counts['map'][rank[1]]
-            if (cutoff):
-                sort = compute_cutoff(cutoff, sort, spectrum, metric, effort)
-            output(sort, spectrum, weff, top1, perc_at_n, prec_rec, collapse,
-                   decimals=decimals)
+            if (args.cutoff_strategy):
+                sort = compute_cutoff(args.cutoff_strategy, sort,
+                                      spectrum, args.metric, args.cutoff_eval)
+            output(sort, spectrum, args.weff, args.top1, args.perc_at_n,
+                   args.prec_rec, args.collapse, decimals=args.decimals)
 
 
 if __name__ == "__main__":
