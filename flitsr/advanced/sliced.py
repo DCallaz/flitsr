9b1d9cde6a732bd8296d5f2c0a2c952f1fc5ea60
Dylan Callaghan <21831599@sun.ac.za>
2025-05-28 17:05:32 +0200

    Changed Sliced back to using other spectrum.
    [Details]
    Changed the Sliced advanced type back to using the sliced spectrum as
    the FLITSR input, and the original spectrum as the parameter. The reason
    for this is because this way around makes more sense as the user would
    be running on the sliced spectrum.
    
    I may change this back again :)
    
    [File's changed]
    	modified:   advanced/sliced.py

--- flitsr/advanced/sliced.py
+++ flitsr/advanced/sliced.py
@@ -9,12 +9,13 @@ from flitsr.input import read_spectrum
 
 class Sliced(Refiner):
     @existing('method', 'split', 'input')
-    def __init__(self, spectrum: str, split: bool, method: bool, input: str):
+    def __init__(self, unsliced_spectrum: str, split: bool, method: bool,
+                 input: str):
         abs_input = osp.abspath(input)
         pth = osp.dirname(abs_input)
         bsnm, ext = osp.splitext(osp.basename(abs_input))
-        conv_spectrum = spectrum.format(pth=pth, bsnm=bsnm, ext=ext)
-        self.sliced_spectrum = read_spectrum(conv_spectrum, split, method)
+        conv_spectrum = unsliced_spectrum.format(pth=pth, bsnm=bsnm, ext=ext)
+        self.unsliced_spectrum = read_spectrum(conv_spectrum, split, method)
 
     def find_test(self, spectrum, test_name: 'str'):
         """ Search for a test with the given name in the given spectrum """
@@ -33,15 +34,16 @@ class Sliced(Refiner):
             search_name = match.group(1)
         return search_name
 
-    def refine(self, spectrum: Spectrum, method_lvl=False) -> Spectrum:
-        print("Refining spectrum!")
-        sb = SpectrumUpdater(self.sliced_spectrum)
-        for failed in self.sliced_spectrum.failing():
+    def refine(self, sliced_spectrum: Spectrum, method_lvl=False) -> Spectrum:
+        # print("Refining spectrum!")
+        sb = SpectrumUpdater(sliced_spectrum)
+        for failed in sliced_spectrum.failing():
             name = self.get_name(failed)
-            old_test = self.find_test(spectrum, name)
+            old_test = self.find_test(self.unsliced_spectrum, name)
             if (old_test is not None and old_test.outcome is Outcome.PASS):
-                print(failed, "is not failing in original")
+                # print(failed, "is not failing in original")
                 sb.remove_test_with_executions(failed)
-                sb.copy_test_and_execution(old_test, spectrum[old_test])
-        print("Finished refining!")
+                sb.copy_test_and_execution(old_test,
+                                           self.unsliced_spectrum[old_test])
+        # print("Finished refining!")
         return sb.get_spectrum()
