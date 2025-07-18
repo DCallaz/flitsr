9a4f068982741c7be530ad54b6e9c21a43aea108
Dylan Callaghan <21831599@sun.ac.za>
2025-07-18 19:33:52 +0200

    Allowed plugins for inputs.
    [Details]
    Added the ability to define plugins for FLITSR input types. This is done
    in a similar way as for advanced types, using the 'flitsr.input'
    entry-point.
    
    [File's changed]
    	modified:   __init__.py

--- flitsr/input/__init__.py
+++ flitsr/input/__init__.py
@@ -1,5 +1,6 @@
 import importlib
 import pkgutil
+import sys
 from enum import Enum
 from flitsr import input
 
@@ -10,9 +11,18 @@ def register_input(cls):
     _inputs[cls.__name__.upper()] = cls
 
 
+# load local inputs
 __all__ = [m[1] for m in pkgutil.iter_modules(input.__path__)]
 for module in __all__:
     importlib.import_module('.'+module, package=__name__)
+# load plugin inputs
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+adv_entry_points = entry_points(group='flitsr.input')
+for adv_ep in adv_entry_points:
+    adv_ep.load()
 
 InputType = Enum('InputType', _inputs,  # type:ignore
                  module=input, qualname='input.InputType')
