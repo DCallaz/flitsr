6c4b3b3c530bd39b76feec01501dc9de1e11098a
Dylan Callaghan <21831599@sun.ac.za>
2025-07-29 10:44:24 +0200

    Changed release version & updated README.
    [Details]
    Updated the README to remove most of the content that is now available
    in the documentation. The README now just references the FLITSR docs.
    
    [Note]
    Changed release version to the upcoming 2.3.0.
    
    [File's changed]
    	modified:   README.md
    	modified:   docs/source/conf.py
    	modified:   pyproject.toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 project = 'flitsr'
 copyright = '2025, Dylan Callaghan'
 author = 'Dylan Callaghan'
-release = '2.2.0'
+release = '2.3.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
