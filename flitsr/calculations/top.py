a0e5923fbe859bba5a9b1385543286f3757cbd77
Dylan Callaghan <21831599@sun.ac.za>
2026-09-22 10:20:28 +0200

    Added more accurate names for top/hit/acc calcs.
    [Details]
    Added more names and renamed the hit@x, Top-n, and acc@x calculations.
    Also improved their descriptions so that it is easier to understand what
    they provide.
    
    [File's changed]
    	modified:   top.py

--- flitsr/calculations/top.py
+++ flitsr/calculations/top.py
@@ -11,16 +11,20 @@ def print_name(name: str):
 
 
 @calculation(print_name('one'), 'Display the expected value of finding at '
-             'least one fault in the top `x` elements (elements with the '
-             'highest suspiciousness).', 'one-top', 'hit-at', 'accuracy-at')
+             'least one fault in the top `x` elements of the ranking (i.e. '
+             'elements with the highest suspiciousness). The metric either '
+             'returns 1 (if at least one fault is certainly found in the top '
+             '`x` elements), or else the expected number of faults found.',
+             'one-top', 'one-hit-at', 'one-accuracy-at')
 def one_top_n(ties: Ties, x: int, collapse=False) -> float:
     k = cut_off_exp_val(ties, x, collapse=collapse)
     return min(1.0, k)
 
 
 @calculation(print_name('all'), 'Display the expected value of the total '
-             'number of faults found in the top `x` elements (elements with '
-             'the highest suspiciousness)', 'all-top')
+             'number of faults found in the top `x` elements of the ranking '
+             '(elements with the highest suspiciousness)', 'all-top',
+             'all-hit-at', 'all-accuracy-at', 'top', 'hit-at', 'accuracy-at')
 def all_top_n(ties: Ties, x: int, collapse=False) -> float:
     k = cut_off_exp_val(ties, x, collapse=collapse)
     return k
